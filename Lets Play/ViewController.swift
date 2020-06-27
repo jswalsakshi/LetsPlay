b435fc29a5c3942c82c7b2898fb8f6cb189e709c
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-06-27 23:15:22 +0530

    first commit

--- Lets Play/ViewController.swift
+++ Lets Play/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func btn_firstEmoji(_ sender: UIButton) {
+        self.flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    @IBAction func btn(_ sender: UIButton) {
+        self.flipCard(withEmoji: "🎃", on: sender)
     }
 
-
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
